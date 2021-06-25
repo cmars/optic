2cadbe045a438a63e48ec232f226aea3ac3b42a6
Aidan Cunniffe <aidan@useoptic.com>
2021-06-25 09:00:53 -0400

    updated

---
+++ website/generate/results/browsers.js
@@ -0,0 +1,15 @@
+module.exports = {
+  data: [
+    {
+      name: 'Chrome',
+      slug: 'chrome',
+      path:
+        '/Users/aidancunniffe/Developer/optic2021/optic/website/docs/browsers/chrome.mdx',
+      link: 'document/browsers/chrome',
+      metadata: { title: 'Chrome' },
+    },
+  ],
+  components: {
+    chrome: require('../../docs/browsers/chrome.mdx'),
+  },
+};
