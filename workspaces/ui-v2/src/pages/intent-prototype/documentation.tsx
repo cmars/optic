bb74a2ca63f705b00999eb1359e7961512c9c24c
Jaap van Hardeveld <jaap@jaaprood.nl>
2021-08-24 13:10:32 +0200

    Allow selection of capture provider for adding endpoint and provide placeholder components for each

--- workspaces/ui-v2/src/pages/intent-prototype/documentation.tsx
+++ workspaces/ui-v2/src/pages/intent-prototype/documentation.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Link, Switch, Route, useRouteMatch } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core';
-import { Button } from '@material-ui/core';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardActions,
+} from '@material-ui/core';
 
 import { EndpointName } from '<src>/components';
 
@@ -14,16 +22,51 @@ import {
 
 export default function DocumentationPage() {
   const styles = useStyles();
+  const routeMatch = useRouteMatch();
+  console.log({ routeMatch });
 
   useFetchEndpoints();
   const endpoints = useAppSelector((state) => state.endpoints.results).data
     ?.endpoints;
 
   return (
-    <div className={styles.container}>
-      <Button color="primary" variant="contained">
-        Add Endpoint
-      </Button>
+    <div className={styles.pageContainer}>
+      <Switch>
+        <Route
+          strict
+          path={`${routeMatch.url}/add/debug-capture`}
+          render={(props) => <div>Upload a debug capture</div>}
+        />
+
+        <Route
+          strict
+          path={`${routeMatch.url}/add/other`}
+          render={(props) => (
+            <div>Interested in this capture method? Let us know</div>
+          )}
+        />
+
+        <Route
+          strict
+          path={`${routeMatch.url}/add`}
+          render={(props) => (
+            <CaptureMethodSelector documentationPath={routeMatch.url} />
+          )}
+        />
+
+        <Route
+          render={(props) => (
+            <Button
+              color="primary"
+              to={`${routeMatch.url}/add`}
+              component={Link}
+              variant="contained"
+            >
+              Add Endpoint
+            </Button>
+          )}
+        />
+      </Switch>
 
       {endpoints && endpoints.length > 0 ? (
         <ul className={styles.endpointsList}>
@@ -45,13 +88,101 @@ export default function DocumentationPage() {
   );
 }
 
+function CaptureMethodSelector({
+  documentationPath,
+}: {
+  documentationPath: string;
+}) {
+  const styles = useStyles();
+
+  return (
+    <div>
+      <h2>Choose the source of data Optic can learn from</h2>
+
+      <div className={styles.captureMethods}>
+        <CaptureMethodCard
+          href={`${documentationPath}/add/debug-capture`}
+          title="Debug capture"
+          description="Add a new endpoint through uploading a debug capture"
+        />
+
+        <CaptureMethodCard
+          href={`${documentationPath}/add/other`}
+          title="Observe traffic locally"
+          description="Use the Optic Capture Toolkit to observe traffic from your local
+                API environment"
+        />
+
+        <CaptureMethodCard
+          href={`${documentationPath}/add/other`}
+          title="Import OpenAPI"
+          description="Allow Optic to learn by uploading your existing OpenAPI spec"
+        />
+      </div>
+    </div>
+  );
+}
+
+function CaptureMethodCard({
+  title,
+  description,
+  href,
+}: {
+  title?: string;
+  description?: string;
+  href: string;
+}) {
+  const styles = useStyles();
+
+  return (
+    <Card className={styles.captureMethodCard}>
+      <CardActionArea to={href} component={Link}>
+        <CardContent>
+          <h3>{title}</h3>
+
+          <p>{description}</p>
+        </CardContent>
+      </CardActionArea>
+
+      <CardActions>
+        <Button size="small">Learn More</Button>
+        <Button
+          to={href}
+          component={Link}
+          size="small"
+          color="primary"
+          variant="contained"
+        >
+          Start
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
 const useStyles = makeStyles((theme) => ({
-  container: {
+  pageContainer: {
     padding: theme.spacing(3, 4),
   },
 
   endpointsList: {
     listStyleType: 'none',
     padding: 0,
+  },
+
+  captureMethods: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+
+  captureMethodCard: {
+    maxWidth: 400,
+    marginRight: theme.spacing(2),
+
+    '& h3': {
+      marginTop: 0,
+    },
   },
 }));
